60a14d70af03d4c9877c833b3922a3c990bd707b
Crowdbotics <team@crowdbotics.com>
2021-03-04 21:07:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/plain_lab_dev_20173/MainActivity.java
@@ -0,0 +1,15 @@
+package com.plain_lab_dev_20173;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "plain_lab_dev_20173";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://plain-lab-dev-20173.botics.co",
+  defaultTimeout: 5000
+};
